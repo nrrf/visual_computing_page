3fd33d8a9c857498e9c8888951dc5b2c01b2cc43
nrrf <nrrf@users.noreply.github.com>
2022-04-06 02:38:14 +0000

    deploy: ace2ef9b24feef2e52e727ffa45d172207690a4b

--- sketches/penrose_triangle.js
+++ sketches/penrose_triangle.js
@@ -1,168 +1,112 @@
-// cubo.js  
-// Para ejecutar en https://editor.p5js.org/
+//penrose_triangle.js
 
-let slider;
-
-
+let sf = 1; // scaleFactor
+let locked = true;
 function setup() {
-  sliderx = createSlider(0, 10, 10,0.1);
+  sliderx = createSlider(0, 10, 10, 0.1);
   sliderx.position(30, 30);
- 
+
   slidery = createSlider(0, 10, 3.6, 0.1);
   slidery.position(30, 55);
- 
+
   sliderz = createSlider(0, 10, 8.1, 0.1);
   sliderz.position(30, 80);
- 
-    createCanvas(700, 700, WEBGL);
-    fill(237, 34, 93);
 
-    //strokeWeight(3);
+  createCanvas(700, 700, WEBGL);
+  fill(237, 34, 93);
+
+  cuboid1 = new Cuboid(0, 180, 0, 30, 0, 30);
+  cuboid2 = new Cuboid(0, 30, 0, 30, 180);
+  cuboid3 = new Cuboid(0, 30, 0, 120, 150, 30);
 }
 
 function draw() {
+  scale(sf);
   background(200);
   rotateX(sliderx.value());
   rotateY(slidery.value());
   rotateZ(sliderz.value());
 
- 
-  // first cube
-  beginShape();
-  vertex(0, 0, 0);
-  vertex(180, 0, 0);
-  vertex(180, 30, 0);
-  vertex(0, 30, 0);
-  endShape(CLOSE);
- 
-  beginShape();
-  vertex(0, 0, 30);
-  vertex(180, 0, 30);
-  vertex(180, 30, 30);
-  vertex(0, 30, 30);
-  endShape(CLOSE);
- 
-  beginShape();
-  vertex(180, 0, 0);
-  vertex(0, 0, 0);
-  vertex(0, 0, 30);
-  vertex(180, 0, 30);
-  endShape(CLOSE);
- 
-  beginShape();
-  vertex(0, 30, 0);
-  vertex(180, 30, 0);
-  vertex(180, 30, 30);
-  vertex(0, 30, 30);
-  endShape(CLOSE);
- 
-  beginShape();
-  vertex(0, 0, 0);
-  vertex(0, 0, 30);
-  vertex(0, 30, 30);
-  vertex(0, 30, 0);
-  endShape(CLOSE);
- 
-  beginShape();
-  vertex(180, 0, 0);
-  vertex(180, 0, 30);
-  vertex(180, 30, 30);
-  vertex(180, 30, 0);
-  endShape(CLOSE);
-
-  // second cube
- 
-  beginShape();
-  vertex(0, 0, 0);
-  vertex(0, 0, 180);
-  vertex(0, 30, 180);
-  vertex(0, 30, 0);
-  endShape(CLOSE);
- 
-  beginShape();
-  vertex(30, 0, 0);
-  vertex(30, 0, 180);
-  vertex(30, 30, 180);
-  vertex(30, 30, 0);
-  endShape(CLOSE);
- 
-  beginShape();
-  vertex(0, 0, 180);
-  vertex(0, 0, 0);
-  vertex(30, 0, 0);
-  vertex(30, 0, 180);
-  endShape(CLOSE);
- 
-  beginShape();
-  vertex(0, 30, 0);
-  vertex(0, 30, 180);
-  vertex(30, 30, 180);
-  vertex(30, 30, 0);
-  endShape(CLOSE);
- 
-  beginShape();
-  vertex(0, 0, 0);
-  vertex(30, 0, 0);
-  vertex(30, 30, 0);
-  vertex(0, 30, 0);
-  endShape(CLOSE);
- 
-  beginShape();
-  vertex(0, 0, 180);
-  vertex(30, 0, 180);
-  vertex(30, 30, 180);
-  vertex(0, 30, 180);
-  endShape(CLOSE);
- 
-  //third cube  
-  beginShape();
-  vertex(0, 0, 180);
-  vertex(0, 0, 150);
-  vertex(30, 0, 150);
-  vertex(30, 0, 180);
-  endShape(CLOSE);
- 
-  beginShape();
-  vertex(0, 120, 180);
-  vertex(0, 120, 150);
-  vertex(30, 120, 150);
-  vertex(30, 120, 180);
-  endShape(CLOSE);
- 
-  beginShape();
-  vertex(0, 0, 180);
-  vertex(0, 0, 150);
-  vertex(0, 120, 150);
-  vertex(0, 120, 180);
-  endShape(CLOSE);
- 
-  beginShape();
-  vertex(30, 0, 180);
-  vertex(30, 0, 150);
-  vertex(30, 120, 150);
-  vertex(30, 120, 180);
-  endShape(CLOSE);
- 
-  beginShape();
-  vertex(0, 0, 180);
-  vertex(0, 120, 180);
-  vertex(30, 120, 180);
-  vertex(30, 0, 180);
-  endShape(CLOSE);
- 
-  beginShape();
-  vertex(0, 0, 150);
-  vertex(0, 120, 150);
-  vertex(30, 120, 150);
-  vertex(30, 0, 150);
-  endShape(CLOSE);
- 
-  beginShape();
-  vertex(0, 120, 180);
-  vertex(0, 105, 150);
-  vertex(30, 105, 150);
-  vertex(30, 120, 180);
-  endShape(CLOSE);
+  // first cuboid 
+  cuboid1.draw()
+  //second cuboid
+  cuboid2.draw()
+  //third cuboid
+  cuboid3.draw()
 
 }
 
+class Cuboid {
+  constructor(x, w, y, h, z, d) {
+    this.x = x
+    this.w = w
+    this.y = y
+    this.h = h
+    this.z = z
+    this.d = d
+  }
+
+  draw() {
+    
+    beginShape();
+
+    vertex(this.x, this.y, this.z);
+    vertex(this.x + this.w, this.y, this.z);
+    vertex(this.x + this.w, this.y + this.h, this.z);
+    vertex(this.x, this.y + this.h, this.z);
+    endShape(CLOSE);
+
+    beginShape();
+    vertex(this.x, this.y, this.z + this.d);
+    vertex(this.x + this.w, this.y, this.z + this.d);
+    vertex(this.x + this.w, this.y + this.h, this.z + this.d);
+    vertex(this.x, this.y + this.h, this.z + this.d);
+    endShape(CLOSE);
+
+    beginShape();
+    vertex(this.x + this.w, this.y, this.z);
+    vertex(this.x, this.y, this.z);
+    vertex(this.x, this.y, this.z + this.d);
+    vertex(this.x + this.w, this.y, this.z + this.d);
+    endShape(CLOSE);
+
+    beginShape();
+    vertex(this.x, this.y + this.h, this.z);
+    vertex(this.x + this.w, this.y + this.h, this.z);
+    vertex(this.x + this.w, this.y + this.h, this.z + this.d);
+    vertex(this.x, this.y + this.h, this.z + this.d);
+    endShape(CLOSE);
+
+    beginShape();
+    vertex(this.x, this.y, this.z);
+    vertex(this.x, this.y, this.z + this.d);
+    vertex(this.x, this.y + this.h, this.z + this.d);
+    vertex(this.x, this.y + this.h, this.z);
+    endShape(CLOSE);
+
+    beginShape();
+    vertex(this.x + this.w, this.y, this.z);
+    vertex(this.x + this.w, this.y, this.z + this.d);
+    vertex(this.x + this.w, this.y + this.h, this.z + this.d);
+    vertex(this.x + this.w, this.y + this.h, this.z);
+    endShape(CLOSE);
+  }
+}
+
+function mousePressed() {
+  locked = false
+}
+
+function mouseClicked() {
+  //console.log('Released')
+  locked = true
+}
+
+function mouseWheel(event) {
+  if (locked != true) {
+    if (event.deltaY < 0)
+      sf *= 1.05;
+    else
+      sf *= 0.95;
+  }
+}
---
+++ sketches/penrose_triangle_2d.js
@@ -0,0 +1,40 @@
+function setup() {
+    createCanvas(400, 400);
+   
+  }
+  
+  function draw() {
+    background(0);
+   
+    stroke(175)
+   
+    noFill()
+   
+    strokeWeight(4)
+   
+    beginShape()
+    vertex(187,100)
+    vertex(100,217)
+    vertex(118,235)
+    vertex(281,235)
+    vertex(292,217)
+    vertex(209,100)
+    vertex(188,100)
+    vertex(255,198)
+    vertex(223,198)
+    //stroke(175)
+    vertex(170,198)
+    vertex(197,162)
+    vertex(224,198)
+    vertex(185,145)
+    vertex(118,235)
+    endShape()
+   
+    beginShape()
+    vertex(170,198)
+    vertex(157,217)
+    vertex(292,217)
+    endShape()
+   
+   
+  }
